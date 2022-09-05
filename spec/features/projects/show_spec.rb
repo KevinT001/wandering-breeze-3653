8e990b035f6b9439423f37bc4fa58bde9fc12c41
Kevin Ta <kvnta@yahoo.com>
2022-09-05 17:01:42 -0600

    Create user story 1 test for project showpage. Create Controller and route for project show page

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'projects show page' do 
+  it "shows project's name and material" do 
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    visit"/projects/#{lit_fit.id}"
+
+    expect(page).to have_content("Litfit")
+    expect(page).to have_content("Lamp")
+
+
+  end
+
+  it 'shows the theme of the challange that the project belongs to' do 
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    visit"/projects/#{lit_fit.id}"
+
+   expect(page).to have_content("Apartment Furnishings")
+
+
+  end
+end
